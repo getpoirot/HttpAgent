813edd448e8fbe068c1cf0bcf2879e107eca8feb
Payam Naderi <naderi.payam@gmail.com>
2015-12-19 16:10:44 +0330

    implement prepare connection

--- HttpPlatform.php
+++ HttpPlatform.php
@@ -5,9 +5,26 @@ use Poirot\ApiClient\Interfaces\iConnection;
 use Poirot\ApiClient\Interfaces\iPlatform;
 use Poirot\ApiClient\Interfaces\Request\iApiMethod;
 use Poirot\ApiClient\Interfaces\Response\iResponse;
+use Poirot\Http\Message\HttpRequest;
+use Poirot\Http\Message\HttpResponse;
+use Poirot\HttpAgent\Interfaces\iHttpTransporter;
+use Poirot\HttpAgent\Transporter\StreamHttpTransporter;
 
 class HttpPlatform implements iPlatform
 {
+    /** @var Browser */
+    protected $browser;
+
+    /**
+     * Construct
+     *
+     * @param $browser
+     */
+    function __construct(Browser $browser)
+    {
+       $this->browser = $browser;
+    }
+
     /**
      * Prepare Connection To Make Call
      *
@@ -15,14 +32,43 @@ class HttpPlatform implements iPlatform
      * - manipulate header or something in connection
      * - get connect to resource
      *
-     * @param iConnection $connection
+     * @param StreamHttpTransporter|iConnection $connection
      *
      * @throws \Exception
-     * @return void
+     * @return StreamHttpTransporter|iHttpTransporter
      */
     function prepareConnection(iConnection $connection)
     {
-        // TODO: Implement prepareConnection() method.
+        $brwOptions = $this->browser->inOptions();
+
+        $reConnect = false;
+
+        # check if we have something changed in connection options
+        if ($conOptions = $brwOptions->getConnection())
+            foreach($conOptions->props()->readable as $prop) {
+                if (
+                    ## not has new option or it may changed
+                    !$connection->inOptions()->__isset($prop)
+                    || ($connection->inOptions()->__get($prop) !== $conOptions->__get($prop))
+                ) {
+                    $connection->inOptions()->__set($prop, $conOptions->__get($prop));
+                    $reConnect = true;
+                }
+            }
+
+        # base url as connection server_url option
+        $baseUrl = $this->browser->inOptions()->getBaseUrl();
+        if ($baseUrl->toString() !== $connection->inOptions()->getServerUrl()) {
+            $connection->inOptions()->setServerUrl($baseUrl);
+            $reConnect = true;
+        }
+
+
+        ## disconnect old connection to reconnect with newly options if has
+        if ($connection->isConnected() && $reConnect)
+            $connection->close();
+
+        return $connection;
     }
 
     /**
@@ -31,11 +77,13 @@ class HttpPlatform implements iPlatform
      *
      * @param iApiMethod $method Method Interface
      *
-     * @return mixed
+     * @return HttpRequest
      */
     function makeExpression(iApiMethod $method)
     {
-        // TODO: Implement makeExpression() method.
+        $request = (new HttpRequest(['method' => 'GET', 'host' => 'raya-media.com']));
+
+        return $request;
     }
 
     /**
@@ -44,13 +92,14 @@ class HttpPlatform implements iPlatform
      * - Result must be compatible with platform
      * - Throw exceptions if response has error
      *
-     * @param mixed $result Server Result
+     * @param HttpResponse $result Server Result
      *
      * @throws \Exception
      * @return iResponse
      */
     function makeResponse($result)
     {
-        // TODO: Implement makeResponse() method.
+        $result->flush();
+        die();
     }
 }
